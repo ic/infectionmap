f872f623e003b9aa5509a82bb37575b192b5434b
Eric Platon <eric.platon@waku-waku.ne.jp>
2014-04-13 04:13:46 +0900

    Work on the UI.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  before_action :expose_agent
+
+  def expose_agent
+    @agent = request.user_agent
+  end
 end
