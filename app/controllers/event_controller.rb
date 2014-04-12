096e318ff0e8362b4cebec1f6e66cf9ea90e4bb9
Eric Platon <eric.platon@waku-waku.ne.jp>
2014-04-13 03:30:32 +0900

    Event submission from the main page.

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,12 +1,13 @@
 class EventController < ApplicationController
 
   def create
+    logger.info params
     e = Event.new
-    e.ip = request.ip
+    e.ip = request.ip.to_s
     e.longitude = params[:lon]
     e.latitude  = params[:lat]
     unless e.save
-      logger.warn("Failed to create event with: params=#{params.inspect}, IP=#{request.ip}")
+      logger.warn("Failed to create event with: params=#{params.inspect}, IP=#{request.ip}, err=#{e.errors.inspect}")
     end
     respond_to do |format|
       format.html { redirect_to :root }
