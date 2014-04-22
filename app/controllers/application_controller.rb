bb87971487053819db090cc48d6a3ad508272647
Eric Platon <eric.platon@waku-waku.ne.jp>
2014-04-23 01:41:59 +0900

    Internationalization for the current views.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   before_action :expose_agent
+  before_action :set_locale
 
   def expose_agent
     @agent = request.user_agent
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 
   def self.redis
@@ -15,6 +20,11 @@ class ApplicationController < ActionController::Base
 
   def redis
     self.redis
+  end
+
+  def default_url_options(opt = {})
+    logger.debug "default_url_options ignores: #{opt.inspect}"
+    { locale: I18n.locale }
   end
 
 end
