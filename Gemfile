4861ebd3f603d298eb124cd152e82c3a8aaec206
Eric Platon <eric.platon@waku-waku.ne.jp>
2015-01-12 12:15:54 +0900

    Introduced Devise. Not used yet.

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'therubyracer',  platforms: :ruby
 
 gem 'haml-rails'
 
+gem 'devise'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   get 'disclaimer' => 'home#disclaimer'
   get 'faq' => 'home#faq'
 
-  root 'home#index'
+  root to: 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
